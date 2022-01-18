13625508dd5806d661ac059da4ab6c8d33cc9e92
Borjaarmet <borjaarmet@gmail.com>
2022-01-18 12:05:22 +0100

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      console.log(tours);
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <h1 className="loading">
+          <Loading />
+        </h1>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <h2>
+        <Tours />
+      </h2>
+    </main>
+  );
 }
 
-export default App
+export default App;
